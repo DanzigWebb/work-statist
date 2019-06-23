4336ae12b4fc21827b9d0cd4e1ed9c48a5695ca9
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-06-23 12:30:59 +0300

    c

--- assets/js/myScripts.js
+++ assets/js/myScripts.js
@@ -1,17 +1,4 @@
 window.onload = function () {
-  let cart = {};
-  let nameAM = {};
-  let info = document.getElementById('hello');
-  // загрузка из корзины
-  function loadCartFromStorage() {
-    // проверка существует ли массив при загрузке страницы
-    if (localStorage.getItem('cart') !== null) {
-      cart = JSON.parse(localStorage.getItem('cart'));
-    } else {
-    }
-  }
-
-  loadCartFromStorage();
 
   // посылаем запрос к таблице
   let getJson = function (url, callback) {
@@ -39,49 +26,91 @@ window.onload = function () {
 
     function arrowHelper(arr) {
 
-      let arrNames = {
-        am: 'Саша М.'
-      }
-
       // фильтрация массива
       let personFilter = function (param, name) {
         return arr.filter(function (el) {
           return el[`gsx$${param}`]['$t'] == name;
         })
       }
-  
 
 
-      let nameAM = personFilter ('исполнитель', 'Саша М.');
-      let nameAS = personFilter ('исполнитель', 'Саша Ш.');
-      let nameMK = personFilter ('исполнитель', 'Майк');
-      let nameAK = personFilter ('исполнитель', 'Леша');
-      let nameRU = personFilter ('исполнитель', 'Рустам');
-      let nameNN = personFilter ('исполнитель', 'Никита');
 
-
-      console.log(`для лидрока: ${personFilter('длякого', 'Leadrock').length} из: ${arr.length} (${personFilter('длякого', 'Leadrock').length / arr.length * 100}%)`);
-      console.log(`для товарки: ${personFilter('длякого', 'Наша товарка').length} из: ${arr.length} (${personFilter('длякого', 'Наша товарка').length / arr.length * 100}%)`);
-      console.log(`для NAT: ${personFilter('длякого', 'Native').length} из: ${arr.length} (${personFilter('длякого', 'Native').length / arr.length * 100}%)`);
-
+      let nameAM = personFilter('исполнитель', 'Саша М.');
+      let nameAS = personFilter('исполнитель', 'Саша Ш.');
+      let nameMK = personFilter('исполнитель', 'Майк');
+      let nameAK = personFilter('исполнитель', 'Леша');
+      let nameRU = personFilter('исполнитель', 'Рустам');
+      let nameNN = personFilter('исполнитель', 'Никита');
 
       let nameHelper = function (name) {
         return name[0]['gsx$исполнитель']['$t']
       };
-      document.getElementById('nameAM').innerHTML = `<p>${nameHelper(nameAM)}: <span>${nameAM.length}</span></p> <p>${nameAM.length/arr.length*100}</p>`;
-      document.getElementById('nameAS').innerHTML = `<p>${nameHelper(nameAS)}: <span>${nameAS.length}</span></p> <p>${nameAS.length/arr.length*100}</p>`;
-      document.getElementById('nameMK').innerHTML = `<p>${nameHelper(nameMK)}: <span>${nameMK.length}</span></p> <p>${nameMK.length/arr.length*100}</p>`;
-      document.getElementById('nameAK').innerHTML = `<p>${nameHelper(nameAK)}: <span>${nameAK.length}</span></p> <p>${nameAK.length/arr.length*100}</p>`;
-      document.getElementById('nameNN').innerHTML = `<p>${nameHelper(nameNN)}: <span>${nameNN.length}</span></p> <p>${nameNN.length/arr.length*100}</p>`;
-      document.getElementById('nameRU').innerHTML = `<p>${nameHelper(nameRU)}: <span>${nameRU.length}</span></p> <p>${nameRU.length/arr.length*100}</p>`;
+      document.getElementById('nameAM').innerHTML = `<p>${nameHelper(nameAM)}: <span>${nameAM.length}</span></p> <p>${nameAM.length / arr.length * 100}</p>`;
+      document.getElementById('nameAS').innerHTML = `<p>${nameHelper(nameAS)}: <span>${nameAS.length}</span></p> <p>${nameAS.length / arr.length * 100}</p>`;
+      document.getElementById('nameMK').innerHTML = `<p>${nameHelper(nameMK)}: <span>${nameMK.length}</span></p> <p>${nameMK.length / arr.length * 100}</p>`;
+      document.getElementById('nameAK').innerHTML = `<p>${nameHelper(nameAK)}: <span>${nameAK.length}</span></p> <p>${nameAK.length / arr.length * 100}</p>`;
+      document.getElementById('nameNN').innerHTML = `<p>${nameHelper(nameNN)}: <span>${nameNN.length}</span></p> <p>${nameNN.length / arr.length * 100}</p>`;
+      document.getElementById('nameRU').innerHTML = `<p>${nameHelper(nameRU)}: <span>${nameRU.length}</span></p> <p>${nameRU.length / arr.length * 100}</p>`;
 
+      let ctx = document.getElementById('main_graph').getContext('2d');
+      let chart_AM = document.getElementById('chart_AM').getContext('2d');
+      let chart_AS = document.getElementById('chart_AS').getContext('2d');
+      let chart_MK = document.getElementById('chart_MK').getContext('2d');
+      let chart_AK = document.getElementById('chart_AK').getContext('2d');
 
-      console.log(nameHelper(nameAM) + ": " + nameAM.length);
-      console.log(nameHelper(nameAS) + ": " + nameAS.length);
-      console.log(nameHelper(nameMK) + ": " + nameMK.length);
-      console.log(nameHelper(nameAK) + ": " + nameAK.length);
-      console.log(nameHelper(nameNN) + ": " + nameNN.length);
-      console.log(nameHelper(nameRU) + ": " + nameRU.length);
+      let chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'doughnut',
+
+        // The data for our dataset
+        data: {
+          labels: [nameHelper(nameAM), nameHelper(nameAS), nameHelper(nameMK), nameHelper(nameAK), nameHelper(nameNN), nameHelper(nameRU)],
+          datasets: [{
+            label: 'Статистика количества выполненных задач',
+
+            backgroundColor: ['orange', 'blue', 'red', 'green', 'yellow', 'pink'],
+            // borderColor: 'rgb(255, 99, 132)',
+            data: [nameAM.length, nameAS.length, nameMK.length, nameAK.length, nameNN.length, nameRU.length]
+          }]
+        },
+
+        // Configuration options go here
+        options: {
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            },
+            width: 400
+          }
+        }
+      });
+
+      let paintGraf = function (id, name) {
+        return new Chart(id, {
+          // The type of chart we want to create
+          type: 'doughnut',
+
+          // The data for our dataset
+          data: {
+            labels: [nameHelper(name), 'Все задачи'],
+            datasets: [{
+              backgroundColor: ['orange', 'blue'],
+              data: [name.length, arr.length]
+            }]
+          },
+          // Configuration options go here
+          options: {}
+        });
+      }
+
+      paintGraf(chart_AM, nameAM)
+      paintGraf(chart_AS, nameAS)
+      paintGraf(chart_MK, nameMK)
+      paintGraf(chart_AK, nameAK)
+
     }
 
   });
